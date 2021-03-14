3213065b7cb2d189e681b9eed05e1c424c4f8e0f
Justin <justin.quaintance@synauta.com>
2021-03-14 06:50:35 +0000

    refactor to players.py

--- main.py
+++ main.py
@@ -5,29 +5,9 @@ import random
 from itertools import combinations, permutations
 import math
 
+import players
+
 # https://github.com/niklasf/python-chess
-
-
-def constraint_value(bool_chk, value):
-    if bool_chk:
-        return value
-    return 0
-
-
-def key_max_val(d):
-    """a) create a list of the dict's keys and values;
-    b) return the key with the max value"""
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(max(v))]
-
-
-def key_min_val(d):
-    """a) create a list of the dict's keys and values;
-    b) return the key with the max value"""
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(min(v))]
 
 
 def print_board(board):
@@ -70,12 +50,6 @@ def print_board(board):
     print(board_string)
 
 
-def random_move(board):
-    """Return a random move from the board"""
-
-    return random.choice(list(board.legal_moves))
-
-
 def game_loop(white, black, wait=0, printing=True):
     """Main game loop for the game
 
@@ -111,155 +85,13 @@ def game_loop(white, black, wait=0, printing=True):
     return board.result()
 
 
-class Player:
-    def __init__(self):
-        self.elo = 400
-
-    def __str__(self):
-        return str(type(self).__name__)
-
-    def get_move(self, board):
-        """take input and get a move for the player"""
-
-        move, level_eval = self.min_max(board)
-        return move
-
-    def min_max(
-        self, board, search_depth=4, max_player=True, alpha=-math.inf, beta=math.inf
-    ):
-
-        best_move = None
-        best_eval = math.inf
-        if max_player:
-            best_eval = best_eval * -1
-
-        for move in board.legal_moves:
-
-            level_eval = 0
-
-            if search_depth != 0:
-                board.push(move)
-                _, level_eval = self.min_max(
-                    board, search_depth=search_depth - 1, max_player=not max_player
-                )
-
-                _ = board.pop()
-
-            try:
-                eval_value = self.eval_move(move, board) + level_eval
-            except TypeError:
-                print(self.eval_move(move, board))
-                print(level_eval)
-
-            if max_player:
-                if best_eval < eval_value:
-                    best_eval = eval_value
-                    best_move = move
-            else:
-                if best_eval > eval_value:
-                    best_eval = eval_value
-                    best_move = move
-
-        return best_move, best_eval
-
-
-class HumanPlayer(Player):
-    def get_move(self, board):
-        """take input and get a move for the player"""
-        error = False
-        move = None
-
-        while move is None:
-
-            if error:
-                prefix = "❌"
-            elif board.turn == chess.WHITE:
-                prefix = "♙"
-            elif board.turn == chess.BLACK:
-                prefix = "♟"
-            input_string = f"{prefix} Please Enter a move \n"
-
-            move_san = input(input_string)
-            try:
-                move = board.parse_san(move_san)
-            except ValueError:
-                error = True
-
-        return move
-
-
-class RandomPlayer(Player):
-    def get_move(self, board):
-        """take input and get a move for the player"""
-
-        return random_move(board)
-
-
-class CapturePlayer(Player):
-    """This player will make random moves but will always capture"""
-
-    def get_move(self, board):
-        """take input and get a move for the player"""
-        checking_move = [
-            move for move in list(board.legal_moves) if board.gives_check(move)
-        ]
-
-        if len(checking_move) > 0:
-            return random.choice(checking_move)
-
-        capture_moves = [
-            move for move in list(board.legal_moves) if board.is_capture(move)
-        ]
-
-        if len(capture_moves) > 0:
-            return random.choice(capture_moves)
-
-        return random_move(board)
-
-
-class BasicMinMaxPlayer(Player):
-    """This player will make random moves but will always capture"""
-
-    def eval_move(self, move, current_board):
-        """Get a value for the goodness of a board
-
-        Return:
-            int
-        """
-
-        eval_number = 0
-
-        eval_number += constraint_value(current_board.gives_check(move), 1000)
-
-        eval_number += constraint_value(current_board.is_capture(move), 1000)
-
-        # Add some randomness
-        eval_number += random.randint(-1, 1)
-
-        current_board.push(move)
-
-        eval_number += constraint_value(current_board.is_checkmate(), 1000)
-        # white = constraint_value(current_board.is_checkmate(), -1e9)
-
-        # If the board is checkmate then we should
-        # if current_board.turn == chess.WHITE:
-        # black = constraint_value(current_board.is_checkmate(), 1e9)
-        # white = constraint_value(current_board.is_checkmate(), -1e9)
-
-        # if current_board.turn == chess.BLACK:
-        # white = constraint_value(current_board.is_checkmate(), 1e9)
-        # black = constraint_value(current_board.is_checkmate(), -1e9)
-        current_board.pop()
-        return eval_number
-
-
 def basic_game():
 
     # black = RandomPlayer()
     # white = CapturePlayer()
 
-    white = HumanPlayer()
-    black = HumanPlayer()
+    white = players.HumanPlayer()
+    black = players.HumanPlayer()
 
     result = game_loop(white, black, wait=0.01)
 
@@ -274,8 +106,8 @@ def basic_game():
 def tournament(games_in_match=3, wait=0):
     all_players = [
         # RandomPlayer(),
-        CapturePlayer(),
-        BasicMinMaxPlayer(),
+        players.CapturePlayer(),
+        players.BasicMinMaxPlayer(),
     ]
 
     bracket = list(
---
+++ players.py
@@ -0,0 +1,159 @@
+import chess
+
+import math
+import random
+
+
+def constraint_value(bool_chk, value):
+    if bool_chk:
+        return value
+    return 0
+
+
+def random_move(board):
+    """Return a random move from the board"""
+
+    return random.choice(list(board.legal_moves))
+
+
+class Player:
+    def __init__(self):
+        self.elo = 400
+
+    def __str__(self):
+        return str(type(self).__name__)
+
+    def get_move(self, board):
+        """take input and get a move for the player"""
+
+        move, level_eval = self.min_max(board)
+        return move
+
+    def min_max(
+        self, board, search_depth=2, max_player=True, alpha=-math.inf, beta=math.inf
+    ):
+        # TODO: Actually add ab pruning here
+
+        best_move = None
+        best_eval = math.inf
+        if max_player:
+            best_eval = best_eval * -1
+
+        for move in board.legal_moves:
+
+            level_eval = 0
+
+            if search_depth != 0:
+                board.push(move)
+                _, level_eval = self.min_max(
+                    board, search_depth=search_depth - 1, max_player=not max_player
+                )
+
+                _ = board.pop()
+
+            try:
+                eval_value = self.eval_move(move, board) + level_eval
+            except TypeError:
+                print(self.eval_move(move, board))
+                print(level_eval)
+
+            if max_player:
+                if best_eval < eval_value:
+                    best_eval = eval_value
+                    best_move = move
+            else:
+                if best_eval > eval_value:
+                    best_eval = eval_value
+                    best_move = move
+
+        return best_move, best_eval
+
+
+class HumanPlayer(Player):
+    def get_move(self, board):
+        """take input and get a move for the player"""
+        error = False
+        move = None
+
+        while move is None:
+
+            if error:
+                prefix = "❌"
+            elif board.turn == chess.WHITE:
+                prefix = "♙"
+            elif board.turn == chess.BLACK:
+                prefix = "♟"
+            input_string = f"{prefix} Please Enter a move \n"
+
+            move_san = input(input_string)
+            try:
+                move = board.parse_san(move_san)
+            except ValueError:
+                error = True
+
+        return move
+
+
+class RandomPlayer(Player):
+    def get_move(self, board):
+        """take input and get a move for the player"""
+
+        return random_move(board)
+
+
+class CapturePlayer(Player):
+    """This player will make random moves but will always capture"""
+
+    def get_move(self, board):
+        """take input and get a move for the player"""
+        checking_move = [
+            move for move in list(board.legal_moves) if board.gives_check(move)
+        ]
+
+        if len(checking_move) > 0:
+            return random.choice(checking_move)
+
+        capture_moves = [
+            move for move in list(board.legal_moves) if board.is_capture(move)
+        ]
+
+        if len(capture_moves) > 0:
+            return random.choice(capture_moves)
+
+        return random_move(board)
+
+
+class BasicMinMaxPlayer(Player):
+    """This player will make random moves but will always capture"""
+
+    def eval_move(self, move, current_board):
+        """Get a value for the goodness of a board
+
+        Return:
+            int
+        """
+
+        eval_number = 0
+
+        eval_number += constraint_value(current_board.gives_check(move), 1000)
+
+        eval_number += constraint_value(current_board.is_capture(move), 1000)
+
+        # Add some randomness
+        eval_number += random.randint(-1, 1)
+
+        current_board.push(move)
+
+        eval_number += constraint_value(current_board.is_checkmate(), 1000)
+        # white = constraint_value(current_board.is_checkmate(), -1e9)
+
+        # If the board is checkmate then we should
+        # if current_board.turn == chess.WHITE:
+        # black = constraint_value(current_board.is_checkmate(), 1e9)
+        # white = constraint_value(current_board.is_checkmate(), -1e9)
+
+        # if current_board.turn == chess.BLACK:
+        # white = constraint_value(current_board.is_checkmate(), 1e9)
+        # black = constraint_value(current_board.is_checkmate(), -1e9)
+        current_board.pop()
+        return eval_number
