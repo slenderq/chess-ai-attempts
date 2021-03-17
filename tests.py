b14ffca0786b58bf48b2f2ec94512960729be97b
Justin <justin.quaintance@synauta.com>
2021-03-17 05:06:14 +0000

    way to benchmark the code

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@ import unittest
 import players
 import chess
 
+from time import time
+
 
 class UtilitesTest(unittest.TestCase):
     # https://www.chessvideos.tv/chess-diagram-generator.php
@@ -22,10 +24,21 @@ class UtilitesTest(unittest.TestCase):
                 self.assertEqual(actual, count)
 
     def test_min_max(self):
+
+        start = time()
+
         board = chess.Board(fen="6k1/2Q5/1P3R1R/p4P1P/5p1p/1PPP1r1r/2q5/6K1")
-        player = players.BasicMinMaxPlayer(search_depth=2)
+        player = players.BasicMinMaxPlayer(search_depth=4)
         # player = players.BetterMinMaxPlayer(search_depth=2)
+
+        end = time()
+
+        process_time = end - start
 
         move = player.get_move(board)
 
-        self.assertEqual(move.uci(), "c7d8")
+        with self.subTest():
+            self.assertEqual(move.uci(), "c7d8")
+
+        with self.subTest():
+            self.assertLessEqual(process_time, 1e-5)
