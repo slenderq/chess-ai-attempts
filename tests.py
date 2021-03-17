25fd1d5e970c11960baae00d765cd195fb8cd9e4
Justin <justin.quaintance@synauta.com>
2021-03-17 05:28:59 +0000

    lru does not work

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import players
 import chess
 
+import main
 from time import time
 
 
@@ -25,20 +26,27 @@ class UtilitesTest(unittest.TestCase):
 
     def test_min_max(self):
 
-        start = time()
-
         board = chess.Board(fen="6k1/2Q5/1P3R1R/p4P1P/5p1p/1PPP1r1r/2q5/6K1")
         player = players.BasicMinMaxPlayer(search_depth=4)
         # player = players.BetterMinMaxPlayer(search_depth=2)
-
-        end = time()
-
-        process_time = end - start
 
         move = player.get_move(board)
 
         with self.subTest():
             self.assertEqual(move.uci(), "c7d8")
 
-        with self.subTest():
-            self.assertLessEqual(process_time, 1e-5)
+        # with self.subTest():
+
+    def test_min_max_perf(self):
+
+        white = players.BasicMinMaxPlayer(search_depth=2)
+        black = players.BasicMinMaxPlayer(search_depth=2)
+        # black = players.BetterMinMaxPlayer(search_depth=3)
+        # black = players.HumanPlayer()
+
+        start = time()
+        main.game_loop(white, black, wait=0)
+        end = time()
+
+        process_time = end - start
+        self.assertLessEqual(process_time, 30)
