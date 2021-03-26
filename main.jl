2af56c6758c86b1007c2590dd13854c159d257a3
Justin Quaintance <justinquaintance@gmail.com>
2021-03-26 18:57:10 +0000

    tournament framework done!

--- main.jl
+++ main.jl
@@ -3,12 +3,42 @@
 using Chess
 # https://romstad.github.io/Chess.jl/dev/
 # https://docs.julialang.org/en/v1/manual/types/#Composite-Types-1
-println("Hello!")
 
-struct RandomPlayer
-    color::PieceColor
-    function RandomPlayer(color)
-        new(color)
+mutable struct RandomPlayer
+
+    elo::Float32
+    function RandomPlayer(elo)
+        new(elo)
+    end
+end
+
+mutable struct HumanPlayer
+    # player::BasicPlayer
+    elo::Float32 
+
+    function HumanPlayer(elo)
+        new(elo)
+    end
+
+end
+
+function choice(iterable)
+
+    n = length(iterable)
+
+    idx = rand(1:n)
+
+    m = iterable[idx]
+
+    return m
+
+end
+
+function flip(p::PieceColor)
+    if p == WHITE
+        return BLACK
+    else
+        return WHITE
     end
 end
 
@@ -16,27 +46,13 @@ function makemove(player::RandomPlayer, board::Board)
     mlist = []
     mlist = moves(board)
 
-    n = length(mlist)
-
-    idx = rand(1:n)
-
-    m = mlist[idx]
-
-    println(m)
+    m = choice(mlist)
+  
+    # println(m)
 
     board = domove(board, m)
     return board
     
-
-end
-struct HumanPlayer
-    # elo::Int8
-    # player::BasicPlayer
-    color::PieceColor
-
-    function HumanPlayer(color)
-        new(color)
-    end
 
 end
 
@@ -104,23 +120,140 @@ function print_board(b::Board)
     
     println(board_string)
 end
-function game_loop()
+function game_loop(white, black, printing::Bool)
 
     b = startboard()
 
-    white = HumanPlayer(WHITE)
-    black = RandomPlayer(BLACK)
-    while !gameover(b)
-        run(`clear`)
-        print_board(b)
+    while !isterminal(b)
+        if printing
+            run(`clear`)
+            print_board(b)
+        end
         if sidetomove(b) == WHITE
             b = makemove(white, b)
         elseif sidetomove(b) == BLACK
             b = makemove(black, b)
         end
     end
+    return b
 
 
 end
 
-game_loop()
+function basic_game()
+    white = HumanPlayer(400)
+    black = RandomPlayer(400)
+
+    game_loop(white, black, true)
+end
+
+tournament(games_in_match) = tournament(games_in_match, true)
+
+function tournament(games_in_match, board_printing::Bool)
+
+    allplayers = [RandomPlayer(400), RandomPlayer(600), RandomPlayer(200), RandomPlayer(100)]
+    
+
+    if length(allplayers) % 2 == 1
+        throw(InvalidStateException("must have even players"))
+    end
+
+    braket_history = []
+    current_braket = allplayers
+    while length(current_braket) > 1
+        matches = []
+        for i in 1:(length(current_braket) - 1)
+            push!(matches,[current_braket[i], current_braket[i + 1]])
+        end
+
+        next_braket = []
+        for match in matches
+            score = [0.0, 0.0]
+            
+            println(" Match: $match  ")
+            for game_number in 1:games_in_match
+
+                if choice([true, false])
+                    white = match[1]
+                    black = match[2]
+                else
+                    white = match[2]
+                    black = match[1]
+                end
+
+                println("   Game: $white $black ")
+                b = game_loop(white, black, board_printing)
+
+                draw = false
+                if ischeckmate(b)
+                    winner_color = flip(sidetomove(b))
+
+                    if winner_color == WHITE
+                        winner = white
+                    else
+                        winner = black
+                    end
+                    player_id = findfirst(isequal(winner), match)
+                    score[player_id] += 1
+                    # println("$winner wins game")
+                else
+                    score[1] += 0.5
+                    score[2] += 0.5
+                    # println("Draw Game")
+                end
+
+                
+
+
+            end
+
+            # 2 is the number of
+
+            if score[1] == score[2] 
+                if match[1].elo > match[2].elo
+                    winner_id = 1
+                elseif match[1].elo < match[2].elo
+                    winner_id = 2
+                else
+                    # just let 1 one win
+                    winner_id = 1
+                end
+            else
+                # Someone one the match, find them
+                max = findmax(score)
+                max_value = max[1]
+                winner_id = findfirst(isequal(max_value), score)
+            end
+
+            match_winner = match[winner_id]
+            println(" $match_winner wins match - $score")
+            if winner_id == 1
+                match_loser = match[2]
+            else
+                match_loser = match[1]
+            end
+
+            temp_elo = match[1].elo
+            match[1].elo += (
+                match[1].elo + (400 * (score[1] - score[2]))
+            ) / games_in_match
+            match[2].elo += (temp_elo +
+                             (400 * (score[2] - score[1]))) / games_in_match
+
+            push!(next_braket, match_winner)
+
+        push!(braket_history, matches)
+        current_braket = next_braket
+
+        end
+
+    # println(braket_history)
+    end
+
+    tournament_winner = current_braket[1]
+    println("$tournament_winner wins the tournament")
+end
+
+tournament(3, false)
+
+# basic_game()
