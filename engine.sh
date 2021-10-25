2980fee3edaecddfa80b17e1c6803fdecff4ffbb
Justin Quaintance <justinquaintance@gmail.com>
2021-10-25 02:59:56 +0000

    adding a way to call julia code in a way that the uci protocall can use

---
+++ engine.sh
@@ -0,0 +1,9 @@
+# This file is so we can run the engie with a general uci interface
+# This idea came from here.
+# https://richardanaya.medium.com/tip-make-a-executable-script-from-a-julia-6bd5f9e7aa80
+#!/bin/sh
+#=
+exec julia -O3 "$0" -- $@
+=#
+include("main.jl")
+basic_game()
