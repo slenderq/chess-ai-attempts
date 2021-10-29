812a215668ccfdb1ae322bb265dd5ab160433c7a
Justin Quaintance <justinquaintance@gmail.com>
2021-10-29 02:39:52 +0000

    adding some more commands

--- talk.jl
+++ talk.jl
@@ -4,10 +4,15 @@ using Chess
 # Porting this file to julia
 # https://github.com/Patil-Onkar/build-your-own-chess-bot/blob/main/communication.py
 
+include("players.jl")
+
 function talk()
+
 
     b = startboard()
     g = Game(b)
+
+    player = TimerMiniMaxPlayer(400, 1, 14)
 
     (errorRead, errorWrite) = redirect_stderr()
     while true
@@ -16,7 +21,7 @@ function talk()
         print(">>> ")
         print(msg)
         print("\n")
-        command(board, msg);
+        b = command(b, msg, player);
 
 
     end
@@ -25,7 +30,7 @@ function talk()
     talk()
 end
 
-function command(board, msg)
+function command(board, msg, player)
     if msg == "quit"
         exit(86)
     end
@@ -33,16 +38,36 @@ function command(board, msg)
     if msg == "uci"
         println("Author Justin Quaintance")
         println("uciok")
-        return
+        return board
     end
 
     if msg == "isready"
         println("readyok")
-        return
+        return board
     end
 
     if msg == "ucinewgame"
-        return 
+        return board
+    end
+
+    if "position startpos moves" in msg
+
+    end
+
+    if "position fen" in msg
+        parts = split(msg, " ")
+        fen = join(parts[2:length(parts)], " ")
+        board = fromfen(fen)
+        return board
+    end
+
+    if startswith(msg, "go")
+        board = makemove(player, board)
+        move = lastmove(board)
+        print("bestmove ")
+        println(tostring(move))
+        return board
+
     end
 
 end
