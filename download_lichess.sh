e0a6f39014e67dbc8d46d60fcd49f745d195a820
Justin Quaintance <justinquaintance@gmail.com>
2021-11-06 06:47:22 +0000

    forgot to add some files

---
+++ download_lichess.sh
@@ -0,0 +1,3 @@
+
+wget -O pgns/justin-games.pgn https://lichess.org/api/games/user/PlantDroom?tags=true&clocks=false&evals=false&opening=false
+wget -O pgns/bot-games.pgn https://lichess.org/api/games/user/QuaintanceBot?tags=true&clocks=false&evals=false&opening=false
